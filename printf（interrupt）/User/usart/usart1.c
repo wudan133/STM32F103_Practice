4e6db704c7a26c5f4141ed072e2eeed0f1dd2f4e
danwu <dan.wu@macrosilicon.com>
2018-07-24 09:51:57 +0800

    Add printf (interrupt) sample

---
+++ printf（interrupt）/User/usart/usart1.c
@@ -0,0 +1,83 @@
+/**
+  ******************************************************************************
+  * @file    usart1.c
+  * @author  
+  * @version V1.0
+  * @date    2013-xx-xx
+  * @brief   usart”¶”√bsp
+  ******************************************************************************
+  */
+#include "usart1.h"
+
+static void NVIC_Configuration(void)
+{
+	NVIC_InitTypeDef NVIC_InitStructure; 
+	/* Configure the NVIC Preemption Priority Bits */  
+	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_0);
+	
+	/* Enable the USARTy Interrupt */
+	NVIC_InitStructure.NVIC_IRQChannel = macUSART_IRQ;	 
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 1;
+	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+	NVIC_Init(&NVIC_InitStructure);
+}
+
+
+ /**
+  * @brief  
+  * @param
+  * @retval
+  */
+void USARTx_Config(void)
+{
+	GPIO_InitTypeDef GPIO_InitStructure;
+	USART_InitTypeDef USART_InitStructure;
+	
+	/* config USART1 clock */
+	macUSART_APBxClock_FUN(macUSART_CLK, ENABLE);
+	macUSART_GPIO_APBxClock_FUN(macUSART_GPIO_CLK, ENABLE);
+	
+	/* USART1 GPIO config */
+	/* Configure USART1 Tx (PA.09) as alternate function push-pull */
+	GPIO_InitStructure.GPIO_Pin =  macUSART_TX_PIN;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(macUSART_TX_PORT, &GPIO_InitStructure);    
+	/* Configure USART1 Rx (PA.10) as input floating */
+	GPIO_InitStructure.GPIO_Pin = macUSART_RX_PIN;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+	GPIO_Init(macUSART_RX_PORT, &GPIO_InitStructure);
+	
+	/* USART1 mode config */
+	USART_InitStructure.USART_BaudRate = macUSART_BAUD_RATE;
+	USART_InitStructure.USART_WordLength = USART_WordLength_8b;
+	USART_InitStructure.USART_StopBits = USART_StopBits_1;
+	USART_InitStructure.USART_Parity = USART_Parity_No ;
+	USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+	USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
+	USART_Init(macUSARTx, &USART_InitStructure);
+	
+	NVIC_Configuration();
+	USART_ITConfig(macUSARTx, USART_IT_RXNE, ENABLE);
+	
+	USART_Cmd(macUSARTx, ENABLE);
+}
+
+
+int fputc(int ch, FILE *f)
+{
+    USART_SendData(macUSARTx, (uint8_t) ch);
+
+    while (USART_GetFlagStatus(macUSARTx, USART_FLAG_TXE) == RESET);		
+
+    return (ch);
+}
+
+int fgetc(FILE *f)
+{
+    while (USART_GetFlagStatus(macUSARTx, USART_FLAG_RXNE) == RESET);
+
+    return (int)USART_ReceiveData(macUSARTx);
+}
+/*********************************************END OF FILE**********************/
